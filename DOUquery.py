0446289c0a08e6f6692410c245c1f35e02eb12de
Bruno Policarpo <brunopolicarpo42@gmail.com>
2016-04-07 22:13:37 -0300

    Adicionado capacidade de repetir queries Adicionado capacidade de definir periodicadade das queries

--- DOUquery.py
+++ DOUquery.py
@@ -10,9 +10,10 @@ import datetime
 import sys
 
 from bs4 import BeautifulSoup
-from time import time
+from time import time,sleep
 from email.mime.text import MIMEText
 from datetime import date,timedelta
+from getpass import getpass
 
 debug = False
 
@@ -47,15 +48,26 @@ parser.add_argument('--inidate', nargs='?', help='initial date for the search', 
 
 parser.add_argument('--enddate', nargs='?', help='end date for the search', default = date.today().strftime("%d/%m"), type=str)
 
+parser.add_argument('--periodic',action='store_true',help='repeat the query')
+
+parser.add_argument('--period',default=60,help='sets the period for query repetition (in seconds)',type=int)
+
+parser.add_argument('--noemail',action='store_true',help='dont send email, print results on stdout')
+
 args = parser.parse_args()
 
 sys.stdout.write("Email to send notification: " + str(args.email) + "\n")
-sys.stdout.write("Start date to search: " + str(args.inidate) + "\n")
-sys.stdout.write("End date to search: " + str(args.enddate) + "\n")
 
-if args.enddate<args.inidate:
-	sys.stderr.write("Error: initial date is greater than end date\n")
-	sys.exit(-1)
+if args.periodic:
+	sys.stdout.write("Making periodic queries\n")
+else:
+	sys.stdout.write("Start date to search: " + str(args.inidate) + "\n")
+	sys.stdout.write("End date to search: " + str(args.enddate) + "\n")
+
+	if args.enddate<args.inidate:
+		sys.stderr.write("Error: initial date is greater than end date\n")
+		sys.exit(-1)
+
 
 url = "http://portal.imprensanacional.gov.br"
 
@@ -74,102 +86,113 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 # User-Agent
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-sys.stdout.write("Starting queries ...\n")
-
 message = ''
-total_matches = 0
 
-output_html = BeautifulSoup('<html><body></body></html>','html.parser') 
-
-for query in args.queries:
-	sys.stdout.write("Querying \"" + query + "\" ... ")
-
-	br.open(url)
-	br.select_form(name='pesquisaAvancada')
-
-	#print br.form
-
-	br['edicao.txtPesquisa']=query
-	br['edicao.dtInicio']=args.inidate
-	br['edicao.dtFim']=args.enddate
-	br.find_control("edicao.jornal").items[0].selected=True
-	response1 = br.submit()
-
-	html = BeautifulSoup(response1.get_data(),'html.parser')
-	print html.original_encoding
-	#html.prettify()
-
-	prog = re.compile('[0-9]+')
-	
-	for div in html.find_all('div'):
-		if 'id' in div.attrs.keys() and div['id']=='paginacao':
-			s = ''.join([ x for x in div.strings])
-			results = prog.search(s)
-			if results!=None:
-				links = compile_links(html)
-				matches = int(results.group(0))			
-			else:
-				links = [ ]
-				matches = 0	
-
-			total_matches+=matches
-
-			header = "\tEncontradas " + str(matches) + " ocorrencias para a busca \" " + query + "\"\n"
-
-			#print output_html.prettify()
-
-			output_html.body.append(header)
-			output_html.body.append(output_html.new_tag("br"))
-
-			#print output_html.prettify()
-
-			for (link,desc) in links:
-				output_html.body.append(link)
-				output_html.body.append(output_html.new_tag("br"))
-				for d in desc:
-					output_html.body.append(d)
-				output_html.body.append(output_html.new_tag("br"))
-
-			sys.stdout.write( str(matches) + " matches \n"  )
-			
-	if debug:
-		print message
-
-#print output_html.prettify()
-
-if not debug:
+if not args.noemail:
 	login = raw_input("Enter your gmail login: ")
-	password = raw_input("Enter your password: ")
+	password = getpass("Enter your password: ")
 
-	sys.stdout.write("Sending email ... ")
+while True:
+	total_matches = 0
+	output_html = BeautifulSoup('<html><body></body></html>','html.parser') 
 
-	msg = MIMEText(str(output_html),'html')
+	sys.stdout.write("Starting queries ...\n")
 
-	# me == the sender's email address
-	# you == the recipient's email address
-	me = login
-	you = ', '.join(args.email)
+	for query in args.queries:
+		sys.stdout.write("Querying \"" + query + "\" ... ")
 
-	print you
+		br.open(url)
+		br.select_form(name='pesquisaAvancada')
 
-	msg['Subject'] = "DOUbot: encontradas " + str(total_matches) + "ocorrencias"
-	msg['From'] = me
-	msg['To'] = ', '.join(args.email)
+		#print br.form
 
-	# Send the message via our own SMTP server, but don't include the
-	# envelope header.
-	# s = smtplib.SMTP('localhost')
-	s = smtplib.SMTP('smtp.gmail.com', 587)
+		br['edicao.txtPesquisa']=query
+		br['edicao.dtInicio']=args.inidate
+		br['edicao.dtFim']=args.enddate
+		br.find_control("edicao.jornal").items[0].selected=True
+		response1 = br.submit()
 
-	s.ehlo()
+		html = BeautifulSoup(response1.get_data(),'html.parser')
+		#print html.original_encoding
+		#html.prettify()
 
-	s.starttls()
+		prog = re.compile('[0-9]+')
+	
+		for div in html.find_all('div'):
+			if 'id' in div.attrs.keys() and div['id']=='paginacao':
+				s = ''.join([ x for x in div.strings])
+				results = prog.search(s)
+				if results!=None:
+					links = compile_links(html)
+					matches = int(results.group(0))			
+				else:
+					links = [ ]
+					matches = 0	
 
-	s.login(login,password)
-	r = s.sendmail(me, you, msg.as_string())
+				total_matches+=matches
 
-	s.quit()
+				header = "\tEncontradas " + str(matches) + " ocorrencias para a busca \" " + query + "\"\n"
 
-	sys.stdout.write("done\n")
+				#print output_html.prettify()
+
+				output_html.body.append(header)
+				output_html.body.append(output_html.new_tag("br"))
+
+				#print output_html.prettify()
+
+				for (link,desc) in links:
+					output_html.body.append(link)
+					output_html.body.append(output_html.new_tag("br"))
+					for d in desc:
+						output_html.body.append(d)
+					output_html.body.append(output_html.new_tag("br"))
+
+				sys.stdout.write( str(matches) + " matches \n"  )
+			
+		if debug:
+			print message
+
+	#print output_html.prettify()
+
+	if not args.noemail:
+		sys.stdout.write("Sending email ... ")
+
+		msg = MIMEText(str(output_html),'html')
+
+		# me == the sender's email address
+		# you == the recipient's email address
+		me = login
+		you = ', '.join(args.email)
+
+		msg['Subject'] = "DOUbot: encontradas " + str(total_matches) + "ocorrencias"
+		msg['From'] = me
+		msg['To'] = ', '.join(args.email)
+
+		# Send the message via our own SMTP server, but don't include the
+		# envelope header.
+		# s = smtplib.SMTP('localhost')
+		s = smtplib.SMTP('smtp.gmail.com', 587)
+
+		s.ehlo()
+
+		s.starttls()
+
+		s.login(login,password)
+		r = s.sendmail(me, you, msg.as_string())
+
+		s.quit()
+
+		sys.stdout.write("done\n")
+	else:
+		sys.stdout.write(output_html.prettify())
+		
+
+	if not args.periodic:		
+		break
+	else:
+		sys.stdout.write("Sleeping for " +str(args.period)+ " seconds ... \n")
+		sys.stdout.flush()
+		sleep(args.period)
+		
 		
 
